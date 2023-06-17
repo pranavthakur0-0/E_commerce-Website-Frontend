a5feee550a8aa40caf049fbadab608680f45e2ef
pranavthakur0-0 <pranavthakurdew@gmail.com>
2023-06-17 14:00:17 +0530

    17June 2023 v3

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,13 @@
 import './Home.scss'
 import Navbar from '../../components/Navbar'
 import Slider from '../../components/Slider'
-import HomeCategories from '../../components/HomeCategories.jsx'
+//import HomeCategories from '../../components/HomeCategories.jsx'
 import SeasonBanner from '../../components/SeasonBanner.jsx'
 import NewArrival from '../../components/NewArrival.jsx'
 import Footer from "../../components/Footer.jsx"
 import Discover from "../../components/Discover.jsx"
 import Sides from "../../components/sides.jsx"
-import Something from "./Something.jsx"
+//import Something from "./Something.jsx"
 import White_back from "./White_back.jsx"
 import WeDesignLogo from "./WeDesignLogo.jsx"
  
