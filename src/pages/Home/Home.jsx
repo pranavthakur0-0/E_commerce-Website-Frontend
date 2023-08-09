f0cedb09870c0dbb64ef11dab2554225c5bcefe8
pranavthakur0-0 <pranavthakurdew@gmail.com>
2023-08-09 21:23:38 +0530

    Redirectsbranch

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ import Footer from "../../components/Footer.jsx"
 import Discover from "../../components/Discover.jsx"
 import Sides from "../../components/sides.jsx"
 //import Something from "./Something.jsx"
-import WhiteBack from "./White_back.jsx"  // Updated import and component name
+import WhiteBack from "./White_back.jsx"  
 import WeDesignLogo from "./WeDesignLogo.jsx"
  
 export default function Home() {
